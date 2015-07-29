051b377bb3b6339f57b76c37e8366036d6e5accf
Sarin Kizhakkepurayil <sarink@juniper.net>
2015-07-28 17:51:56 -0700

    Related-Bug: #1470231 re-commiting code leftout after previous merge 1. this is for showing cluster replication factor.

--- webroot/monitor/storage/ui/js/views/ClusterUsageView.js
+++ webroot/monitor/storage/ui/js/views/ClusterUsageView.js
@@ -4,8 +4,9 @@
 
 define([
     'underscore',
-    'contrail-view'
-], function (_, ContrailView) {
+    'contrail-view',
+    'contrail-list-model'
+], function (_, ContrailView, ContrailListModel) {
     var ClusterUsageView = ContrailView.extend({
         el: $(contentContainer),
 
@@ -18,11 +19,63 @@ define([
                 title: swl.TITLE_CLUSTER_USAGE,
                 usageChartId: swl.CLUSTER_USAGE_CHART_ID,
                 replicaFactorId: swl.CLUSTER_REPLICA_FACTOR_ID,
-                replicaFactorTitle: swl.TITLE_CLUSTER_REPLICA_FACTOR,
+                replicaFactorTitle: swl.TITLE_CLUSTER_REPLICA_FACTOR
             }));
+
+            var defaultModelConfig = {
+                modelKey: swc.UMID_CLUSTER_USAGE,
+                remote: {
+                    ajaxConfig: {
+                        url: swc.URL_CLUSTER_USAGE,
+                        type: 'GET'
+                    },
+                    dataParser: swp.clusterUsageDataParser
+                },
+                cacheConfig: {
+                    ucid: swc.UCID_CLUSTER_USAGE
+                },
+                vlRemoteConfig: {
+                    vlRemoteList: [{
+                        getAjaxConfig: function (response) {
+                            var lazyAjaxConfig = {
+                                url: swc.URL_POOLS_SUMMARY,
+                                type: 'GET'
+                            };
+                            return lazyAjaxConfig;
+                        },
+                        dataParser: swp.clusterReplicaFactorParser,
+                        successCallback: function (clusterReplicationFactor, contrailListModel) {
+                            var usageSummary = contrailListModel.getItems();
+                            contrailListModel.updateData(swp.clusterUsageWithReplicaFactor(usageSummary, clusterReplicationFactor))
+                        }
+                    }]
+                }
+            };
+
+            viewConfig.modelConfig = $.extend(true, {}, viewConfig.modelConfig, defaultModelConfig);
+            self.model = new ContrailListModel(viewConfig.modelConfig);
+
+            if (self.model !== null) {
+                if (self.model.loadedFromCache || !(self.model.isRequestInProgress())) {
+                    var chartData = self.model.getItems();
+                    selector = swu.getSelector4Id(swl.CLUSTER_REPLICA_FACTOR_ID);
+                    self.renderReplicaFactor(selector, viewConfig, chartData);
+                }
+
+                self.model.onAllRequestsComplete.subscribe(function () {
+                    var chartData = self.model.getItems();
+                    selector = swu.getSelector4Id(swl.CLUSTER_REPLICA_FACTOR_ID);
+                    self.renderReplicaFactor(selector, viewConfig, chartData);
+                });
+            }
+
             selector = swu.getSelector4Id(swl.CLUSTER_USAGE_CHART_ID);
 
             self.renderView4Config(selector, null, getClusterUsageViewConfig(viewConfig));
+        },
+
+        renderReplicaFactor: function(selector, viewConfig, data) {
+            $(selector).text(data[0]['cluster_replica_factor']);
         }
     });
 
